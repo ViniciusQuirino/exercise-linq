52e0d0fca0b5370286d6ab41689f6e6d7606e8e9
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-07-15 16:57:02 -0300

    linq

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Entities;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter salary: ");
+            double limit = double.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            try
+            {
+
+                using (StreamReader sr = File.OpenText("/home/vinicius/Área de Trabalho/exercise-linq/in.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salaryEmployee = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        list.Add(new Employee(name, email, salaryEmployee));
+                    }
+                }
+
+                var emails = list.Where(obj => obj.Salary > limit).OrderBy(obj => obj.Email).Select(obj => obj.Email);
+
+                var sum = list.Where(obj => obj.Name[0] == 'A').Sum(obj => obj.Salary);
+
+                Console.WriteLine("Email of people whose salary is more than " + limit.ToString("F2", CultureInfo.InvariantCulture));
+                foreach (string email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+
+                Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
